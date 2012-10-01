7e58fb70c6bf1207ef66e72b43dcff16ede9ff2e
Tyler Smith <tyler@uservoice.com>
2012-10-01 10:44:17 -0700

    Initialize Determine::Exceptions

--- lib/determine.rb
+++ lib/determine.rb
@@ -4,7 +4,9 @@ require 'nokogiri'
 
 # Create our root module so we don't need to worry about it later, add ourselves
 # to the load path, and get to work
-module Determine;end
+module Determine
+  module Exceptions;end
+end
 
 $:.unshift File.dirname(__FILE__)
 require 'determine/exceptions/determination_exception'
