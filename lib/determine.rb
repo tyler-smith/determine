3d5af657087544ff83535d4d10006dd284e00b35
Tyler Smith <tyler@uservoice.com>
2012-10-01 16:14:19 -0700

    Reduce complexity with loading of exceptions

--- lib/determine.rb
+++ lib/determine.rb
@@ -3,7 +3,9 @@ require 'nokogiri'
 
 # Create our root module so we don't need to worry about it later, add ourselves
 # to the load path, and get to work
-module Determine;end
+module Determine
+  module Exceptions;end
+end
 
 $:.unshift File.dirname(__FILE__)
-Dir[File.join(File.dirname(__FILE__), 'determine', '*.rb')].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), 'determine', '**','*.rb')].each { |f| require f }
--- lib/determine/exceptions.rb
+++
@@ -1,2 +0,0 @@
-module Determine::Exceptions;end
-Dir[File.join(File.dirname(__FILE__), 'exceptions', '*.rb')].each { |f| require f }
