ba88b39f4b4c3cbf87ac140ccf535511d3e77611
Tyler Smith <tyler@uservoice.com>
2012-10-05 09:44:24 -0700

    Make determinations try 'default' handler if none present

--- lib/determine/determination.rb
+++ lib/determine/determination.rb
@@ -2,16 +2,14 @@ module Determine
   class Determination
     # Accessor for getting our ordered list of handlers
     def handlers
-      @handlers ||= []
+      @handlers ||= ['default']
     end
 
     # Takes the text and tries each handler on it until we get a determination
     # TODO: in future extend this to allow multiple determinations
     def determine(page, *args)
-      raise "No provided methods" if handlers.nil? || handlers.empty?
-
       handlers.each do |handler|
-        result = self.send(handler, page, *args)
+        result = self.send(handler, page, *args) if self.respond_to?(handler)
         return result if result
       end
 
